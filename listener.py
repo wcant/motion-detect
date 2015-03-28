147db82a304a83db0e47504210516294599aed01
Wesley Cantrell <cantrell.wesley@gmail.com>
2015-03-28 13:02:49 -0500

    getting syntax error in cursor.execute(), possibly caused by img_path string

--- listener.py
+++ listener.py
@@ -4,6 +4,11 @@ import RPi.GPIO as GPIO
 import picamera
 import gps
 import time
+import sqlite3
+import os.path
+
+dbcon = sqlite3.connect('motion.db',check_same_thread=False)
+cursor = dbcon.cursor()
 
 camera = picamera.PiCamera()
 
@@ -12,7 +17,7 @@ camera = picamera.PiCamera()
 GPIO.setmode(GPIO.BCM)
 
 #listen on pin 4 for PIR
-PIR_PIN = 4
+PIR_PIN = 22
 
 # Logically Low by default
 GPIO.setup(PIR_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
@@ -28,10 +33,30 @@ session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 def eventTrigger(channel):
 
 	print("Motion Detected")
+
+	#Get time string and remove unfriendly characters
 	time_str = time.ctime()
-	img_path = 'images/'+time_str.replace(" ", "_")+'.jpg'
+	time_str = time_str.replace(" ", "")
+	time_str = time_str.replace(":", "")
+	img_path = '/usr/share/nginx/www/images/'+time_str+'.jpg'
+
 	camera.capture(img_path)
+
+	#get image ready for insert
 	
+	day = time_str[0:3]
+	mon = time_str[3:6]
+	dayn = time_str[6:8]
+	hour = time_str[8:10]
+	minu = time_str[10:12]
+	sec = time_str[12:14]
+	year = time_str[14:19]
+	print(time_str)
+
+#	list_to_write = [day,dayn,mon,year,hour,minu,sec]	
+	cursor.execute("INSERT INTO log VALUES (NULL, "+day+","+dayn+","+mon+","+year+","+hour+","+minu+","+sec+","+"NULL"+","+img_path+");")	
+	cursor.commit()
+	cursor.close()
 	#maybe call logger.py here and add to database
 
 #--------------- Event Listener --------------------------#
@@ -43,7 +68,6 @@ try:
 	
 		print(time.ctime())
 		time.sleep(5)	
-		
 
 except KeyboardInterrupt:
 
